ad38256e43f50a592c1ffb6328c86781c84337d7
zdd <1178315564@qq.com>
2019-04-10 08:43:31 +0800

    some change

--- web/src/router/Api.vue
+++ web/src/router/Api.vue
@@ -3,10 +3,10 @@
 </template>
 
 <script>
-  const Login_Api='http://127.0.0.1:8000' //登录接口
+  const Global_Api='http://127.0.0.1:8000' //登录接口
     export default {
         name: "Api",
-        Login_Api,
+        Global_Api
     }
 </script>
 
