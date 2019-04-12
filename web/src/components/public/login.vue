4cfd26404ec334809ef112128c36a5fb8a087fc9
zdd1998 <39595293+zdd1998@users.noreply.github.com>
2019-04-12 14:08:11 +0800

    Web/ddzhang/init web (#7)
    * 前端初始化
    
    * 前端初始化2
    
    * 删除gitignore中的build对前端代码的影响
    
    * 添加build
    
    * public
    
    * some change
    
    * some change
    
    * 完成了手机端
    
    * test
    
    * 删除全局style
    
    * 修改了一些bug，合并了注册页面
    
    * 注册
    
    * 修改了手机端按钮
    
    * 加了迭代路由
    
    * 基本完成了登录注册
    
    * 修改了通知及返回刷新
    
    * fix bugs

---
+++ web/src/components/public/login.vue
@@ -0,0 +1,128 @@
+<template>
+  <div>
+    <div class="login-wrap">
+      <h3>登录</h3>
+      <input type="text" placeholder="请输入用户名" v-model="username">
+      <input type="password" placeholder="请输入密码" v-model="password">
+      <button v-on:click="login">登录</button>
+      <button class="btn btn-link" @click="register">没有账号？马上注册</button>
+    </div>
+  </div>
+</template>
+
+<style>
+  .login-wrap {
+    text-align: center;
+  }
+
+  input {
+    display: block;
+    width: 250px;
+    height: 40px;
+    line-height: 40px;
+    margin: 0 auto;
+    margin-bottom: 10px;
+    outline: none;
+    border: 1px solid #888;
+    padding: 10px;
+    box-sizing: border-box;
+  }
+
+  p {
+    color: red;
+  }
+
+  button {
+    display: block;
+    width: 250px;
+    height: 40px;
+    line-height: 40px;
+    margin: 0 auto;
+    border: none;
+    background-color: #41b883;
+    color: #fff;
+    font-size: 16px;
+    margin-bottom: 5px;
+  }
+
+  span {
+    cursor: pointer;
+  }
+
+  span:hover {
+    color: #41b883;
+  }
+</style>
+
+<script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+      }
+    },
+    mounted() {
+      if (this.$cookie.get('username'))
+        if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+          this.$router.push({
+            path: '/main_mob',
+          })
+          location.reload()
+        } else {
+          this.$router.push({
+            path: '/main',
+          })
+          location.reload()
+        }
+    },
+    methods: {
+      login: function () {
+        if (this.username == "" || this.password == "") {
+          alert("请输入用户名或密码")
+        } else {
+          let data = {'user_name': this.username, 'user_password': this.password}
+          /*接口请求*/
+          this.$http.post(this.Global_Api + '/user/login', data).then((res) => {
+            if (res.body.error_code !== 0) {
+              alert("登录失败" + res.body.error_message)
+            } else {
+              //alert("登录成功")
+
+              this.$cookie.set('username', this.username, 600);
+              this.$cookie.set('user_token', res.body.user_token, 600);
+              this.$cookie.set('user_type', res.body.user_type, 600);
+
+              if (res.body.user_type === 1) {
+                this.$cookie.set('realname', res.body.teacher.teacher_name, 600);
+                this.$cookie.set('user_teacher_number', res.body.teacher.teacher_number, 600);
+              }
+              else if (res.body.user_type === 2) {
+                this.$cookie.set('realname', res.body.student.student_name, 600);
+                this.$cookie.set('user_student_number', res.body.student.student_number, 600);
+              }
+              else {
+                this.$cookie.set('realname', this.username, 600);
+              }
+              if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+                this.$router.push({
+                  path: '/main_mob',
+                })
+                location.reload()
+              } else {
+                this.$router.push({
+                  path: '/main',
+                })
+                location.reload()
+              }
+            }
+          })
+        }
+      },
+      register: function () {
+        this.$router.push('/register')
+        location.reload()
+      }
+    }
+  }
+</script>
---
+++ web/src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import Resource from 'vue-resource'
+import Api from './router/Api'  //导入全局接口
+import $ from 'jquery'   //引入jQuery
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.min'
+import vSelect from 'vue-select'
+Vue.component('v-select', vSelect)
+Vue.use(Resource)
+Vue.prototype.Global_Api=Api.Global_Api
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+var app = new Vue({
+  el: '#app',
+  router,
+  components: {App},
+  template: '<App/>'
+})
+
+
+
+
+
