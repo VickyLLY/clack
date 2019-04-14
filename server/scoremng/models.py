95cc648ce4a607617cf83351dd98197587ff95a9
yinyukang <949418316@qq.com>
2019-04-14 22:23:53 +0800

    添加了学生成绩的model

--- server/scoremng/models.py
+++ server/scoremng/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from entity.models import Student
+from entity.models import Course
 
-# Create your models here.
+
+# 成绩
+class Score(models.Model):
+    student_number = models.ForeignKey(Student, on_delete=models.CASCADE, null=False)
+    course_number = models.ForeignKey(Course, on_delete=models.CASCADE, null=False)
+    score = models.IntegerField(default=0, null=False)
