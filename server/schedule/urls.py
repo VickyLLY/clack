aba921849ec34407aff05ac650dc38de67505245
Weijie Zhao <maggch@outlook.com>
2019-04-14 17:37:10 +0800

    添加排课子系统的urls.py

---
+++ server/schedule/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from schedule import views
+
+urlpatterns = [
+    path(r'new_course', views.new_course, name='new_course'),
+    path(r'course_list', views.course_list, name='course_list')
+]
