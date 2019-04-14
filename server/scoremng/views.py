18cca313f6850be8df0b0e064fd8163d1ad42aec
yinyukang <949418316@qq.com>
2019-04-14 23:51:33 +0800

    修改了老师上传成绩的函数

--- server/scoremng/views.py
+++ server/scoremng/views.py
@@ -1,14 +1,20 @@
 import json
-from server import error_code
+
+import scoremng.models
+from server import error_code, schema
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from server.decorators import check_json
 
-# Create your views here.
 
-
+@check_json(schema.entity_new_course)
 def teacher_upload(request):
     request_json = json.loads(request.body)
-    # print(request_json)
     # 把新增的学生成绩添加到数据库中
-
+    score = scoremng.models.Score(student_id=request_json['student_id'], course_id=request_json['course_id'],
+                                  score=request_json['score'])
+    try:
+        score.save()
+    except Exception:
+        return JsonResponse({**error_code.CLACK_CREATE_NEW_MODELS_FAILED})
     return JsonResponse({**error_code.CLACK_SUCCESS})
