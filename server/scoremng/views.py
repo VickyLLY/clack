1f393c1eeb7113d0d1f444596066fc4cf9256765
yinyukang <949418316@qq.com>
2019-04-14 22:22:39 +0800

    实现了老师上传学生成绩的api

--- server/scoremng/views.py
+++ server/scoremng/views.py
@@ -1,3 +1,14 @@
+import json
+from server import error_code
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def teacher_upload(request):
+    request_json = json.loads(request.body)
+    # print(request_json)
+    # 把新增的学生成绩添加到数据库中
+
+    return JsonResponse({**error_code.CLACK_SUCCESS})
