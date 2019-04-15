d7bce8f7b44bb6b125ace83217c0f55ca97915aa
zzw <2218455091@qq.com>
2019-04-15 09:43:16 +0800

    修改了手机端总体思想

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -5,9 +5,10 @@ import Register from '@/components/public/register' //注册
 import Main from '@/components/public/main' //PC端主页
 import Add_course from '@/components/schedule/add_course'
 import VueCookie from 'vue-cookie'
-import main_mob from '@/components/schedule/main_mob' //手机端主页
-import schedule_mob from  '@/components/schedule/schedule_mob'
-import news_mob from  '@/components/schedule/news_mob'
+import main_mob from '@/components/mob/main_mob' //手机端主页
+import schedule_mob from  '@/components/mob/schedule_mob'
+import test from '@/components/mob/test'
+import bar_mob from '@/components/mob/bar_mob'
 Vue.use(VueCookie)
 Vue.use(Router)
 
@@ -17,7 +18,10 @@ export default new Router({
       path: '/',
       name: 'Login',
       component: Login,
-
+    },
+    {
+      path:'/test',
+      component: test,
     },
     {
       path: '/register',
@@ -28,25 +32,14 @@ export default new Router({
       path: '/main_mob',
       name: 'main_mob',
       component: main_mob,
-      meta: {
-        keepAlive: true// 需要被缓存
-      },
-      children:[
-        {
-          path:'schedule_mob',
-          component: schedule_mob,
-          meta: {
-            keepAlive: false // 需要被缓存
-          },
-        },
-        {
-          path:'news_mob',
-          component: news_mob,
-          meta: {
-            keepAlive: false // 需要被缓存
-          },
-        }
-      ]
+    },
+    {
+      path: '/bar_mob',
+      component: bar_mob,
+    },
+    {
+      path:'/schedule_mob',
+      component: schedule_mob,
     },
     {
       path: '/main',
