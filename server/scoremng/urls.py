574e9e7ebe37c169e90f1dbde73b009e2e3184c4
yinyukang <949418316@qq.com>
2019-04-21 20:44:47 +0800

    修改了学生查看自己成绩的url

--- server/scoremng/urls.py
+++ server/scoremng/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from scoremng import views
 
 urlpatterns = [
-    path('teacher_upload', views.teacher_upload, name='teacher_upload'),
-    path('student_scores/<str:student_number>', views.student_scores, name='student_scores'),
+    path('teacher_upload/', views.teacher_upload, name='teacher_upload'),
+    path('student_scores/<str:student_number>/', views.student_scores, name='student_scores'),
+    path('courses_comment/<str:student_number>/', views.courses_comment, name='courses_comment'),
 ]
