a95e8e7a55de36fefdaa6de9f22c0c335cbc9daf
yinyukang <949418316@qq.com>
2019-04-23 20:30:30 +0800

    添加了老师查看和下载成绩的url和接口

--- server/scoremng/urls.py
+++ server/scoremng/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('student_scores/<str:student_number>/', views.student_scores, name='student_scores'),
     path('courses_comment/<str:student_number>/', views.courses_comment, name='courses_comment'),
     path('student_download_scores/<str:student_number>/', views.student_download_scores, name='download_scores'),
+    path('teacher_download_scores/<str:teacher_number>/', views.teacher_download_scores, name='teacher_download_scores'),
 ]
--- server/scoremng/views.py
+++ server/scoremng/views.py
@@ -280,3 +280,81 @@ def student_download_scores(request, student_number):
         os.remove(student.student_name + '的成绩单.xls')
     wb.save(student.student_name + '的成绩单.xls')
     return JsonResponse({**error_code.CLACK_SUCCESS, 'score_list': score_list})
+
+
+# 老师下载自己所带的学生的成绩
+def teacher_download_scores(request, teacher_number):
+    """
+    在排课表中查找这名老师带的全部课程的id,
+    在选课表中由课程id，查找到选这门课的学生的id
+    因为先阶段不知道排课表和选课表中的字段设置，所以先给前端返回假数据
+    """
+    year = request.GET.get('year')
+    semester = request.GET.get('semester')
+    # print(year, semester)
+
+    try:
+        teacher = Teacher.objects.get(teacher_number=teacher_number)
+    except Exception:
+        return JsonResponse({**error_code.CLACK_TEACHER_NOT_EXISTS})
+
+    wb = xlwt.Workbook(encoding='utf-8')
+    w = wb.add_sheet(u'学生成绩', cell_overwrite_ok=True)
+    w.write(0, 0, u'学生学号')
+    w.write(0, 1, u'学生姓名')
+    w.write(0, 2, u'学生班级')
+    w.write(0, 3, u'课程名称')
+    w.write(0, 4, u'课程种类')
+    w.write(0, 5, u'课程学年')
+    w.write(0, 6, u'课程学期')
+    w.write(0, 7, u'课程学分')
+    w.write(0, 8, u'课程成绩')
+    excel_row = 1
+
+    student_score_list = []
+
+    student_score_list1 = {
+        'student_number': '2015014069',
+        'student_name': '尹毓康',
+        'student_banji_name': '计科1602班',
+        'course_name': '编译原理',
+        'course_type': 0,
+        'course_year': 2018,
+        'course_semester': 1,
+        'course_credit': 3,
+        'course_score': 90,
+    }
+
+    student_score_list2 = {
+        'student_number': '2016014069',
+        'student_name': '蔡锐',
+        'student_banji_name': '计科1605班',
+        'course_name': '数据结构',
+        'course_type': 0,
+        'course_year': 2019,
+        'course_semester': 1,
+        'course_credit': 3,
+        'course_score': 100,
+    }
+
+    student_score_list.append(student_score_list1)
+    student_score_list.append(student_score_list2)
+
+    # 把成绩单里的学生成绩信息放入excel表中
+    for student_score in student_score_list:
+        w.write(excel_row, 0, student_score['student_number'])
+        w.write(excel_row, 1, student_score['student_name'])
+        w.write(excel_row, 2, student_score['student_banji_name'])
+        w.write(excel_row, 3, student_score['course_name'])
+        w.write(excel_row, 4, student_score['course_type'])
+        w.write(excel_row, 5, student_score['course_year'])
+        w.write(excel_row, 6, student_score['course_semester'])
+        w.write(excel_row, 7, student_score['course_credit'])
+        w.write(excel_row, 8, student_score['course_score'])
+        excel_row += 1
+
+    exist_file = os.path.exists(teacher.teacher_name + '所带学生的成绩单.xls')
+    if exist_file:
+        os.remove(teacher.teacher_name + '所带学生的成绩单.xls')
+    wb.save(teacher.teacher_name + '所带学生的成绩单.xls')
+    return JsonResponse({**error_code.CLACK_SUCCESS, 'student_score_list': student_score_list})
