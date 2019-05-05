8997b0b37bb35db59e149c9c6408b885469a7b9f
O-direction <2537169087@qq.com>
2019-05-05 17:33:05 +0800

    增加了悬浮窗

--- web/src/components/select_course/stu_sel_course.vue
+++ web/src/components/select_course/stu_sel_course.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="overflow: hidden" class="clearfix">
     <nav class="navbar navbar-default navbar-fixed-top">
       <a class="navbar-brand"><strong>教务管理系统</strong></a>
       <ul class="nav navbar-nav">
@@ -7,8 +7,11 @@
         <li><a href="">研究生选课</a></li>
         <li><a href="">查看课表</a></li>
         <li><a href="">其他注意事项</a></li>
-
       </ul>
+      <div id="clock">
+        <p class="date">{{ date }}</p>
+        <p class="time">{{ time }}</p>
+      </div>
       <ul class="nav navbar-nav navbar-right">
         <li class="col-md-7">
         <li class="dropdown">
@@ -23,8 +26,10 @@
         </li>
       </ul>
     </nav>
-    <div id="aside" class="container right"
-         style="width:700px;height:700px;border:1px solid red;position:relative;top:100px;left:1140px;">
+
+
+    <div id="aside" class="container pull-right"
+         style="width:700px;height:700px;border:1px solid red;position:relative;top:100px;left:640px;">
       <div class="row">
         <div class="col-lg-1" style="background: red;height:700px;position: relative;">
           <p style="width: 20px;height: 100px;position:absolute;top:140px;line-height: 30px;">选课信息</p>
@@ -44,135 +49,10 @@
               <td>星期日</td>
             </tr>
             </thead>
-            <tr>
-              <td>1</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>4</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>5</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>6</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>7</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>8</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>9</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>10</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>11</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>12</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>13</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
+            <tr v-for="n in 13">
+              <td :class=td[n]>{{n}}</td>
+              <td v-for="m in 7" class="init">
+              </td>
             </tr>
           </table>
         </div>
@@ -183,47 +63,39 @@
 
 
 <script>
+  import $ from 'jquery'
   export default {
     name: "stu_sel_course",
     data() {
       return {
         username: this.$cookie.get('username'),
         realname: this.$cookie.get('realname'),
+        time: '',
+        date: '',
+        week: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
+        td:['one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen'],
       }
     },
     methods: {
       quit: function () {
         this.$cookie.delete('username');
-      }
-    },
-    mounted: function () {
-      var oDiv1=document.getElementById('aside');
-      alert(oDiv1);
-      oDiv1.onmouseover = function () {
-        startMove(this, 'left',500 );
-
-      };
-      oDiv1.onmouseout = function () {
-        startMove(this, 'left', 1140);
-      };
-
-      function getStyle(obj, name) {
+      },
+      getStyle: function (obj, name) {
         if (obj.currentStyle) {
           return obj.currentStyle[name];
         } else {
           return getComputedStyle(obj, false)[name];
         }
-      }
-
-      function startMove(obj, attr, iTarget) {
+      },
+      startMove: function (obj, attr, iTarget, vueObject) {
         clearInterval(obj.timer);
         obj.timer = setInterval(function () {
           var cur = 0;
 
           if (attr === 'opacity') {
-            cur = parseFloat(getStyle(obj, attr)) * 100;
+            cur = parseFloat(vueObject.getStyle(obj, attr)) * 100;
           } else {
-            cur = parseInt(getStyle(obj, attr));
+            cur = parseInt(vueObject.getStyle(obj, attr));
           }
 
           var speed = (iTarget - cur) / 6;
@@ -241,11 +113,72 @@
             }
           }
         }, 30);
+      },
+      updateTime: function (vueObject) {
+        var cd = new Date();
+        vueObject.time = vueObject.zeroPadding(cd.getHours(), 2) + ':' + vueObject.zeroPadding(cd.getMinutes(), 2) + ':' + vueObject.zeroPadding(cd.getSeconds(), 2);
+        vueObject.date = vueObject.zeroPadding(cd.getFullYear(), 4) + '-' + vueObject.zeroPadding(cd.getMonth() + 1, 2) + '-' + vueObject.zeroPadding(cd.getDate(), 2) + ' ' + vueObject.week[cd.getDay()];
+      },
+      zeroPadding: function (num, digit) {
+        var zero = '';
+        for (let i = 0; i < digit; i++) {
+          zero += '0';
+        }
+        return (zero + num).slice(-digit);
+      },
+
+    },
+    mounted: function () {
+      var that = this;
+      var oDiv1 = document.getElementById('aside');
+      this.$nextTick(() => {
+        if(that.timer){
+          clearInterval(that.timer);
+        }
+        that.timer=setInterval(()=>{
+          that.updateTime(that);
+        },1000);
+        that.updateTime(that);
+        oDiv1.onmouseover = function () {
+          that.startMove(oDiv1, 'left', 10, that);
+        };
+        oDiv1.onmouseout = function () {
+          that.startMove(oDiv1, 'left', 640, that);
+        };
+
+      })
+    },
+    computed:{
+      reColor:function () {
+        
       }
-    }
+    },
   }
 </script>
 
 <style scoped>
+  #clock {
+    font-family: 'Microsoft YaHei','Lantinghei SC','Open Sans',Arial,'Hiragino Sans GB','STHeiti','WenQuanYi Micro Hei','SimSun',sans-serif;
+    color: #2b669a;
+    text-align: center;
+    position: absolute;
+    left: 60%;
+    top:10px;
+    text-shadow: 0 0 20px #2aabd2, 0 0 20px rgba(100, 100, 200, 0.5);
+  }
+  #clock .time {
+    letter-spacing: 0.05em;
+    font-size: 20px;
+    padding: 5px;
+    display: inline-block;
+  }
+  #clock .date {
 
+    display: inline-block;
+    letter-spacing: 0.1em;
+    font-size: 20px;
+  }
+  .init{
+    background: green;
+  }
 </style>
