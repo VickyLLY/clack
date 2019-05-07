90addbd2f22b633a12c1a0c79144785a96717b08
ccchenge <chenge0618@gmail.com>
2019-05-07 20:13:08 +0800

    添加 展示课题列表 展示已审核列表 教师提交的课题 学生自己选择的课题 功能

--- server/dst/urls.py
+++ server/dst/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path(r'view_detail', views.view_detail, name="view_detail"),
     path(r'new_dst', views.new_dst, name="new_dst"),
     path(r'stu_select',views.stu_select,name='stu_select'),
-
+    path(r'dst_list', views.dst_list, name='dst_list'),
+    path(r'dst_list_approval', views.dst_list_approval, name='dst_list_approval'),
+    path(r'teacher_dst_list', views.teacher_dst_list, name='teacher_dst_list'),
+    path(r'stu_dst_list', views.stu_dst_list, name='stu_dst_list'),
 ]
--- server/dst/views.py
+++ server/dst/views.py
@@ -6,7 +6,6 @@ import entity.models
 import dst.models
 from server.decorators import admin_required,login_required
 
-
 # Create your views here.
 @admin_required
 def view_detail(request):
@@ -63,3 +62,88 @@ def stu_select(request):
     except Exception as e:
         return JsonResponse({**error_code.CLACK_UNEXPECTED_ERROR, "exception": str(e)})
     return JsonResponse({**error_code.CLACK_SUCCESS})
+
+@login_required
+def dst_list(request):
+    request_json = json.loads(request.body)
+    dsts = entity.models.DissertationTopic.objects.all()
+    result_list = []
+    for dst in dsts:
+        tname=list(entity.models.Teacher.objects.filter(id=dst.dissertation_tnum_id))
+        tt=tname[0]
+        result_list_te = [{
+            'dissertation_id': dst.id,
+            'dissertation_title': dst.dissertation_title,
+            'dissertation_content': dst.dissertation_content,
+            'dissertation_requirement': dst.dissertation_requirement,
+            'dissertation_capacity': dst.dissertation_capacity,
+            'dissertation_approval': dst.dissertation_approval,
+            'dissertation_pub_time': dst.dissertation_pub_time,
+            'dissertation_teacher': tt.teacher_name,
+        } ]
+        result_list = result_list + result_list_te
+
+    return JsonResponse({**error_code.CLACK_SUCCESS, 'dst_list': result_list})
+
+@login_required
+def dst_list_approval(request):
+    request_json = json.loads(request.body)
+    dsts = entity.models.DissertationTopic.objects.filter(dissertation_approval=True)
+    result_list=[]
+    for dst in dsts:
+        tname=list(entity.models.Teacher.objects.filter(id=dst.dissertation_tnum_id))
+        tt=tname[0]
+        result_list_te = [{
+            'dissertation_id': dst.id,
+            'dissertation_title': dst.dissertation_title,
+            'dissertation_content': dst.dissertation_content,
+            'dissertation_requirement': dst.dissertation_requirement,
+            'dissertation_capacity': dst.dissertation_capacity,
+            'dissertation_pub_time': dst.dissertation_pub_time,
+            'dissertation_teacher': tt.teacher_name,
+        } ]
+        result_list = result_list + result_list_te
+
+    return JsonResponse({**error_code.CLACK_SUCCESS, 'dst_list': result_list})
+
+@login_required
+def teacher_dst_list(request):
+    request_json = json.loads(request.body)
+    query_result = list(
+        entity.models.User.objects.filter(user_name=request_json['user_name']))
+    user = query_result[0]
+    tnum = user.user_teacher_id
+    dsts = entity.models.DissertationTopic.objects.filter(dissertation_tnum_id=tnum)
+    result_list = [{
+        'dissertation_id': dst.id,
+        'dissertation_title': dst.dissertation_title,
+        'dissertation_content': dst.dissertation_content,
+        'dissertation_requirement': dst.dissertation_requirement,
+        'dissertation_capacity': dst.dissertation_capacity,
+        'dissertation_approval':dst.dissertation_approval,
+        'dissertation_pub_time':dst.dissertation_pub_time,
+    } for dst in dsts]
+    return JsonResponse({**error_code.CLACK_SUCCESS, 'teacher_dst_list': result_list})
+
+@login_required
+def stu_dst_list(request):
+    request_json = json.loads(request.body)
+    query_result = list(
+        entity.models.User.objects.filter(user_name=request_json['user_name']))
+    user = query_result[0]
+    snum = user.user_student_id
+    dd=dst.models.Application.objects.filter(student_id=snum)
+    tnum=dd[0].dissertation_id
+    dsts = entity.models.DissertationTopic.objects.filter(id=tnum)
+    tname = list(entity.models.Teacher.objects.filter(id=dsts[0].dissertation_tnum_id))
+    tt = tname[0]
+    result_list = [{
+        'dissertation_id': dsta.id,
+        'dissertation_title': dsta.dissertation_title,
+        'dissertation_content': dsta.dissertation_content,
+        'dissertation_requirement': dsta.dissertation_requirement,
+        'dissertation_capacity': dsta.dissertation_capacity,
+        'dissertation_pub_time': dsta.dissertation_pub_time,
+        'dissertation_teacher': tt.teacher_name,
+    } for dsta in dsts]
+    return JsonResponse({**error_code.CLACK_SUCCESS, 'stu_dst_list': result_list})
