a45f3c6d6ebb667f7001205486f46853a2c2f72d
Mr-Dear <d_muses@163.com>
2019-05-11 16:12:59 +0800

    上传选课子系统

---
+++ server/selecourse/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from entity.models import Student, Course
+# Create your models here.
+
+#选课表
+class Selection(models.Model):
+    #在数据库中分别对应为selection_student_id和selection_course_id,由Django自动生成
+    selection_student=models.ForeignKey(Student,on_delete=models.CASCADE, null=False)
+    selection_course=models.ForeignKey(Course,on_delete=models.CASCADE, null=False)
+
